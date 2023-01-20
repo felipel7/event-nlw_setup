2fff70b7ba14d70a7eb9706e48c12b2618cde6f7
felipel7 <felipelseven@gmail.com>
2023-01-19 23:05:29 -0300

    chore(deps): add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,17 @@
 import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return 'Hello world!';
+app.get('/', async () => {
+  return await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'beb',
+      },
+    },
+  });
 });
 
 app.listen({
