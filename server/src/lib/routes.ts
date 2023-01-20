7514c70fbb05da1e7fc8b1dc5e8954da984a9611
felipel7 <felipelseven@gmail.com>
2023-01-20 20:23:49 -0300

    chore(deps): add zod

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -1,12 +1,28 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
 import { prisma } from './prisma';
 
 async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    return await prisma.habit.findMany({
-      where: {
-        title: {
-          startsWith: 'beb',
+  app.post('/habits', async (req: FastifyRequest, res: FastifyReply) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+
+        weekDays: {
+          create: weekDays.map(week_day => {
+            return {
+              week_day,
+            };
+          }),
         },
       },
     });
