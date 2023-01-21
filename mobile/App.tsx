70c6d9a10614a372470f999450715b57005be7a9
felipel7 <felipelseven@gmail.com>
2023-01-21 00:42:01 -0300

    style(mobile): create Header component structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,10 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [isFontLoaded] = useFonts({
@@ -21,28 +23,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <>
+      <Home />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#e4e4e4',
-    fontFamily: 'Inter_800ExtraBold',
-  },
-});
---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+import { Header } from '../components/Header';
+
+function Home() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16 ">
+      <Header />
+    </View>
+  );
+}
+
+export { Home };
