728f9ac7e4d9b6bd383bee81fd892edbe371afaf
felipel7 <felipelseven@gmail.com>
2023-01-21 20:57:16 -0300

    feat: create a new route to toggle day's habit

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -68,6 +68,58 @@ async function appRoutes(app: FastifyInstance) {
 
     return { dayHabits, completedHabits };
   });
+
+  app.patch(
+    '/habits/:id/toggle',
+    async (req: FastifyRequest, res: FastifyReply) => {
+      const toggleHabit = z.object({
+        id: z.string().uuid(),
+      });
+
+      const { id } = toggleHabit.parse(req.params);
+
+      // startOf remove minutes and hours of the date
+      const today = dayjs().startOf('day').toDate();
+
+      let day = await prisma.day.findUnique({
+        where: {
+          date: today,
+        },
+      });
+
+      if (!day) {
+        day = await prisma.day.create({
+          data: {
+            date: today,
+          },
+        });
+      }
+
+      const dayHabit = await prisma.dayHabit.findUnique({
+        where: {
+          day_id_habit_id: {
+            day_id: day.id,
+            habit_id: id,
+          },
+        },
+      });
+
+      if (dayHabit) {
+        await prisma.dayHabit.delete({
+          where: {
+            id: dayHabit.id,
+          },
+        });
+      } else {
+        await prisma.dayHabit.create({
+          data: {
+            day_id: day.id,
+            habit_id: id,
+          },
+        });
+      }
+    }
+  );
 }
 
 export { appRoutes };
