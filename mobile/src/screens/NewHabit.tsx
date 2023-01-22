ee89f32aad7cf546e0b4865db7bf2f0808d99f36
felipel7 <felipelseven@gmail.com>
2023-01-22 16:54:25 -0300

    chore(mobile): integrate form submission with api

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {
+  Alert,
   ScrollView,
   Text,
   TextInput,
@@ -7,6 +8,7 @@ import {
   View,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { api } from '../lib/axios';
 import colors from 'tailwindcss/colors';
 
 import { BackButton } from '../components/BackButton';
@@ -15,12 +17,37 @@ import { weekDays } from './Home';
 
 function NewHabit() {
   const [selectedDays, setSelectedDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
   function handleToggleSelectedDay(day_id: number) {
     if (selectedDays.includes(day_id)) {
       setSelectedDays(prev => prev.filter(id => id !== day_id));
     } else {
       setSelectedDays(prev => [...prev, day_id]);
+    }
+  }
+
+  async function handleNewHabitCreation() {
+    try {
+      if (!title.trim() || selectedDays.length === 0) {
+        Alert.alert(
+          'You must choose',
+          'the days frequency and the title before you can establish a new habit.'
+        );
+      }
+
+      await api.post('/habits', {
+        title,
+        weekDays: selectedDays,
+      });
+
+      setTitle('');
+      setSelectedDays([]);
+
+      Alert.alert('Success!', 'Your new habit has been created.');
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Sorry', 'Something went wrong...');
     }
   }
 
@@ -44,6 +71,8 @@ function NewHabit() {
           className="h-12 pl-4 mt-3 text-white border-2 rounded-lg bg-zinc-900 focus:border-2 focus:border-green-600 border-zinc-800"
           placeholder="Type your new habit here..."
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={setTitle}
+          value={title}
         />
 
         <Text className="mt-4 mb-3 text-base font-semibold text-white">
@@ -58,7 +87,10 @@ function NewHabit() {
           />
         ))}
 
-        <TouchableOpacity className="flex-row items-center justify-center w-full mt-6 bg-green-600 rounded-md h-14">
+        <TouchableOpacity
+          onPress={handleNewHabitCreation}
+          className="flex-row items-center justify-center w-full mt-6 bg-green-600 rounded-md h-14"
+        >
           <Feather name="check" size={20} color={colors.white} />
           <Text className="ml-2 text-base font-semibold text-white">Send</Text>
         </TouchableOpacity>
