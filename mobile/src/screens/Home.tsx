674e74e3d5facd7275429142b0870173430c2e85
felipel7 <felipelseven@gmail.com>
2023-01-22 02:36:14 -0300

    feat(mobile): create NewHabit screen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -4,14 +4,14 @@ import { Header } from '../components/Header';
 import { DayHabit, SQUARE_DAY_SIZE } from '../components/DayHabit';
 import { generateDateRange } from '../utils/generateDateRange';
 
-const weekDays = [
-  { id: 0, label: 'S' },
-  { id: 1, label: 'M' },
-  { id: 2, label: 'T' },
-  { id: 3, label: 'W' },
-  { id: 4, label: 'T' },
-  { id: 5, label: 'F' },
-  { id: 6, label: 'S' },
+export const weekDays = [
+  { id: 0, label: 'S', name: 'Sunday' },
+  { id: 1, label: 'M', name: 'Monday' },
+  { id: 2, label: 'T', name: 'Tuesday' },
+  { id: 3, label: 'W', name: 'Wednesday' },
+  { id: 4, label: 'T', name: 'Thursday' },
+  { id: 5, label: 'F', name: 'Friday' },
+  { id: 6, label: 'S', name: 'Saturday' },
 ];
 
 const minimumSummaryDatesSizes = 18 * 5;
@@ -20,14 +20,14 @@ const daysOfTheYear = generateDateRange();
 
 function Home() {
   return (
-    <View className="flex-1 bg-background px-8 pt-16 ">
+    <View className="flex-1 px-8 pt-16 bg-background ">
       <Header />
 
       <View className="flex-row mt-6 mb-2">
         {weekDays.map(day => (
           <Text
             key={day.id}
-            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            className="mx-1 text-xl font-bold text-center text-zinc-400"
             style={{
               width: SQUARE_DAY_SIZE,
             }}
@@ -49,7 +49,7 @@ function Home() {
             Array.from({ length: amountDaysToFill }).map((_, i) => (
               <View
                 key={i}
-                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                className="m-1 border-2 rounded-lg bg-zinc-900 border-zinc-800 opacity-40"
                 style={{
                   width: SQUARE_DAY_SIZE,
                   height: SQUARE_DAY_SIZE,
--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,7 +1,69 @@
-import { View } from 'react-native';
+import { useState } from 'react';
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { BackButton } from '../components/BackButton';
+import { Checkbox } from '../components/Checkbox';
+import { weekDays } from './Home';
+import colors from 'tailwindcss/colors';
 
 function NewHabit() {
-  return <View className="flex-1 px-8 pt-16 bg-background">New Habit</View>;
+  const [selectedDays, setSelectedDays] = useState<number[]>([]);
+
+  function handleToggleSelectedDay(day_id: number) {
+    if (selectedDays.includes(day_id)) {
+      setSelectedDays(prev => prev.filter(id => id !== day_id));
+    } else {
+      setSelectedDays(prev => [...prev, day_id]);
+    }
+  }
+
+  return (
+    <View className="flex-1 px-8 pt-16 bg-background">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-3xl font-extrabold text-white">
+          Create a new Habit
+        </Text>
+
+        <Text className="mt-6 text-base font-semibold text-white">
+          Start developing a new habit
+        </Text>
+
+        <TextInput
+          className="h-12 pl-4 mt-3 text-white rounded-lg bg-zinc-800 focus:border-2 focus:border-green-600 "
+          placeholder="Type your new habit here..."
+          placeholderTextColor={colors.zinc[400]}
+        />
+
+        <Text className="mt-4 mb-3 text-base font-semibold text-white">
+          What's the frequency?
+        </Text>
+        {weekDays.map(day => (
+          <Checkbox
+            key={day.id}
+            title={day.name}
+            checked={selectedDays.includes(day.id)}
+            onPress={() => handleToggleSelectedDay(day.id)}
+          />
+        ))}
+
+        <TouchableOpacity className="flex-row items-center justify-center w-full mt-6 bg-green-600 rounded-md h-14">
+          <Feather name="check" size={20} color={colors.white} />
+          <Text className="ml-2 text-base font-semibold text-white">Send</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 }
 
 export { NewHabit };
