b8b9cd28ca683300c890b532bccae5a24ced2c01
felipel7 <felipelseven@gmail.com>
2023-01-21 21:18:48 -0300

    feat: create a new route to get summary table data

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -120,6 +120,25 @@ async function appRoutes(app: FastifyInstance) {
       }
     }
   );
+
+  app.get('/summary', async (req: FastifyRequest, res: FastifyReply) => {
+    const summary = await prisma.$queryRaw`
+      SELECT D.id, D.date, 
+      (
+        SELECT cast(count(*) as float) FROM day_habits DH
+        WHERE DH.day_id = D.id
+      ) as completed,
+      (
+        SELECT cast(count(*) as float) FROM habit_week_days HWD
+        JOIN habits H ON H.id = HWD.habit_id
+        WHERE HWD.week_day = cast(strftime('%w', D.date / 1000.0, 'unixepoch') as int)
+        AND H.created_at <= D.date
+      ) as amount
+      FROM days D
+      `;
+
+    return summary;
+  });
 }
 
 export { appRoutes };
